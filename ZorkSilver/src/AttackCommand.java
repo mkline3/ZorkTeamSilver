bc116d4266c3291e22c337a09b568a7d1f3e5fcb
Nathan <Nathan@NathanPC>
2017-04-05 21:52:08 -0400

    Files added

---
+++ ZorkSilver/src/AttackCommand.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *This allows user to fight Enemies
+ * @author Matt
+ */
+public class AttackCommand {
+    /**
+     * This is where the user selects an item from inventory to use as a weapon
+     * @param weapon 
+     */
+    public AttackCommand(Item weapon){
+        
+    }
+    /**
+     * This will return what happens when user attacks an enemy
+     * @return 
+     */
+    public String execute(){
+        return null;
+    }
+}
