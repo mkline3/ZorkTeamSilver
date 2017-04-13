c076623d0a99adca94327fdfbaef84f065070faf
jordan smith <jordan smith>
2017-04-13 16:18:41 -0400

    Used to add score or take away score

---
+++ AddScore.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jordan.smith
+ */
+public class AddScore implements Events{
+
+    private int value;
+    
+    public AddScore(int value){
+        
+        this.value = value;
+    }
+    public void execute() {
+        GameState.instance().addScore(value);
+        if(value > 1)
+            System.out.println("You gained " + value + " points!");
+        else if(value < 0)
+            System.out.println("You lost " + value + " points!");
+        else if(value == 1)
+            System.out.println("You earned a point for your score!");
+    }
+
+    @Override
+    public boolean hasCalledTimer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setHasCalledTimer(boolean x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
